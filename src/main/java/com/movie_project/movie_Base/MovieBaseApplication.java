c9e6c3b9cf675693e3b7cb2024b03b759d51ea1b
TAIWO-ADEDEJI <tadedeji@systemspecscomng.onmicrosoft.com>
2024-09-19 09:30:13 +0100

    initial files

---
+++ src/main/java/com/movie_project/movie_Base/MovieBaseApplication.java
@@ -0,0 +1,13 @@
+package com.movie_project.movie_Base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie_project/movie_Base/MovieBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie_project.movie_Base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
