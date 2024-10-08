2bf8c3c882776fdd561951e40fbcfd8be6c1b1ee
TAIWO-ADEDEJI <tadedeji@systemspecscomng.onmicrosoft.com>
2024-10-08 13:30:39 +0100

    minor tweaks

--- src/main/java/com/movie_project/movie_Base/Controllers/AuthenticationController.java
+++ src/main/java/com/movie_project/movie_Base/Controllers/AuthenticationController.java
@@ -9,11 +9,9 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
